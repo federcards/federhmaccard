b0dbb315b8016b81d5e82a32ed39e05269801875
NeoAtlantis <contact@chaobai.li>
2022-07-26 22:25:22 +0200

    display tree from csv

--- federhmaccard/gui/PasswordTreeview.py
+++ federhmaccard/gui/PasswordTreeview.py
@@ -8,11 +8,20 @@ class PasswordTreeview(ttk.Treeview):
 
     def __init__(self, parent, csv, *args, **kvargs):
         kvargs["show"] = "tree" 
+        kvargs["columns"] = ["#0", "Description", "URI"]
         ttk.Treeview.__init__(self, parent, *args, **kvargs)
 
         self.ybar = Scrollbar(parent, orient=VERTICAL, command=self.yview)
         self.configure(yscroll=self.ybar.set)
 
+        #self.column("#0")
+        #self.column("Description")
+        #self.column("URI")
+        self.heading("#0", text="Title", anchor="w")
+        self.heading("Description", text="Description", anchor="w")
+        self.heading("URI", text="URI", anchor="w")
+
+        self.rows = {}
         self._load_csv(csv)
 
 
@@ -45,5 +54,17 @@ class PasswordTreeview(ttk.Treeview):
                 parent_path = ("",)
             parent_path_id = path_id[parent_path]
             iid = path_id[path]
-            print(path, parent_path, iid)
             self.insert(parent_path_id, "end", iid=path_id[path], text=path[-1])
+
+        # add items
+
+        for row_i in range(0, len(rows)):
+            row_id = "row-%d" % row_i
+            self.rows[row_id] = rows[row_i]
+            title, uri, path = rows[row_i]
+
+            parent_path_id = path_id[path]
+            self.insert(
+                parent_path_id, "end", iid=row_id,
+                text=title, values=("Alphanumeric",uri)
+            )
